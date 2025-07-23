ac2c01ce5e0b38ab51eb489c388d4740f48da7b4
sellis8 <sellis8@my.waketech.edu>
2025-07-23 11:12:53 -0400

    Update cti_110_node.js

--- cti_110_node.js
+++ cti_110_node.js
@@ -9,9 +9,9 @@ const path = require('path');
 // This part sets up the database
 const {Pool} = require('pg');
 // You may need to modify the password or database name in the following line:
-const connectionString = `postgres://postgres:CTI_110_WakeTech@localhost/Gradebook`;
+const connectionString = `postgres://postgres:CTI_110_WakeTech@localhost/Ellis_Gradebook`;
 // The default password is CTI_110_WakeTech
-// The default database name is Gradebook
+// The default database name is Ellis_Gradebook
 const pool = new Pool({connectionString:connectionString})
 
 // This line says when it's looking for a file linked locally,
@@ -22,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // someone calls "get" on the server root "/"
 router.get('/', function(req, res){
     // It just returns a file to their browser 
-    // from the same directory it's in, called gradebook.html
-    res.sendFile(path.join(__dirname, 'gradebook.html'));
+    // from the same directory it's in, called Ellis_Gradebook.html
+    res.sendFile(path.join(__dirname, 'Ellis_Gradebook.html'));
 });
 
 app.use("/", router);
