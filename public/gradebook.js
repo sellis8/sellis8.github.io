b0e73c8df1c8ed0fd29410aae638a9cb6abe2834
sellis8 <sellis8@my.waketech.edu>
2025-07-25 12:43:32 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,13 +1,52 @@
-// TODO: Fetch data from the PostgreSQL database (to be implemented later)
 function fetchGradeData() {
   // This function will query the PostgreSQL database and return grade data
   console.log("Fetching grade data...");
+  // Create a new request for HTTP data
+  let xhr = new XMLHttpRequest();
+  // This is the address on the machine we're asking for data
+  let apiRoute = "/api/grades";
+  // When the requests changes status, we run this anonymous function
+  xhr.onreadystatechange = function(){
+	let results;
+	// Check if we're done
+	if(xhr.readyState --- xhr.DONE){
+	  // Check if we're successful
+	  if(xhr.status !== 200){
+		console.error('Could not get grades.
+		  Status: ${xhr.status}');
+	  }
+	  // And then call the function to update the HTML with our data
+	  populateGradebook(JSON.parse(xhr.responseText));
+  	  }
+  }.bind(this);
+  xhr.open("get", apiRoute, true);
+  xhr.send();
 }
 
-// TODO: Populate the table with grade data 
 function populateGradebook(data) {
   // This function will take the fetched grade data and populate the table
-  console.log("populateGradebook function called");
+  console.log("Populating gradebook with data:", data);
+  let tableElm = document.getElementbyId("gradebook"); // Get the gradebook table element
+	data.forEach(function(assignment){ // For each row of data we're passed in
+	  let row = document.createElement("tr"); // create a table row element
+	  let columns = []; // Handy place to stick the columns of information 
+	  columns.name = document.createElement('td'); // The first column's table data will be the name 
+	  columns.name.appendChild(
+		// Concatenate the full name: "last_name, first name"
+		document.createTextMode(assignment.last_name + "," + assignment.first_name)
+	  );
+	  columns.grade = document.createElement('td'); // second column will be the grade
+	  columns.grade.appendChild(
+		// Just put the name in text, you could be fancy and figure out the letter grade here
+		// with either a bunch of conditions, or a Javascript "switch" statement
+		document.createTextMode(assignment.total_grade)
+	  );
+	  // Add the table data columns to the table row
+	  row.appendChild(columns.name);
+	  row.appendChild(columns.grade);
+	  // Add the row to the table itself to make the data visible
+	  tableElm.appendChild(row);
+	});
 }
 
 // TODO  REMOVE THIS
